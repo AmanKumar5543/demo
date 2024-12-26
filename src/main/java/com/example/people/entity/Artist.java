c8b95606ad9f5c0e2b33d4b37d2cc4e1a1c87f5b
Amankumar5543 <ashrivashtava@gmail.com>
2024-12-26 22:08:19 +0530

    formatted setlist services

--- src/main/java/com/example/people/entity/Artist.java
+++ src/main/java/com/example/people/entity/Artist.java
@@ -1,9 +1,16 @@
 package com.example.people.entity;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Data
+@Getter
+@Setter
 
 @Entity
 @Table
@@ -18,6 +25,7 @@ public class Artist {
     @JsonManagedReference
     public Name name;
 
+
     @Column(name = "Instrument")
     String instrument;
 
@@ -27,39 +35,4 @@ public class Artist {
     @OneToMany(mappedBy = "artist")
     private List<SetList> setLists;
 
-    public List<SetList> getSetLists() {
-        return setLists;
-    }
-
-    public void setSetLists(List<SetList> setLists) {
-        this.setLists = setLists;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public Name getName() {
-        return name;
-    }
-
-    public void setName(Name name) {
-        this.name = name;
-    }
-
-    public String getInstrument() {
-        return instrument;
-    }
-
-    public void setInstrument(String instrument) {
-        this.instrument = instrument;
-    }
-
-    public int getBookingPrice() {
-        return bookingPrice;
-    }
-
-    public void setBookingPrice(int bookingPrice) {
-        this.bookingPrice = bookingPrice;
-    }
 }
